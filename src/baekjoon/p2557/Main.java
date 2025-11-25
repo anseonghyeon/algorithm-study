90424cde0af36c668de6f5609a2566bdb5b909c3
anseonghyeon <anseonghyeon@gmail.com>
2025-11-25 23:57:23 +0900

    init: 프로젝트 세팅 및 2557번 풀이

---
+++ src/baekjoon/p2557/Main.java
@@ -0,0 +1,14 @@
+package baekjoon.p2557;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("Hello World!");
+        System.out.println(sb);
+    }
+}
